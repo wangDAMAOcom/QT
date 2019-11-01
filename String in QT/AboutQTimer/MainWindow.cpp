5db1bfd804d40c487a0a44c6846569d0b589768f
wangDAMAOcom <34620455+wangDAMAOcom@users.noreply.github.com>
2019-11-01 14:56:40 +0800

    Add files via upload

---
+++ String in QT/AboutQTimer/MainWindow.cpp
@@ -0,0 +1,25 @@
+﻿#include "MainWindow.h"
+#include "ui_MainWindow.h"
+#include <QTime>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    m_timeId = startTimer(1000);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::timerEvent(QTimerEvent *event){
+    if (event->timerId() == m_timeId){
+        QString time = QTime::currentTime().toString("HH::mm::ss");
+        ui->label->setText(time);
+    }
+
+    return ;
+}
---
+++ String in QT/AboutQTimer/MainWindow.h
@@ -0,0 +1,24 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    virtual void timerEvent(QTimerEvent *);
+
+private:
+    Ui::MainWindow *ui;
+    int m_timeId;
+};
+
+#endif // MAINWINDOW_H
